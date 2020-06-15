8855b91c71cf273348947afd6d9a0007f6d1b7b8
Turetska <oturetska@yahoo.com>
2020-06-15 18:53:32 -0400

    created assets folder and script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+var questions = [
+    {
+      title: "Example Question 1:",
+      choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+      answer: "answer from choices"
+    },
+    {
+      title: "Example Question 2:",
+      choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+      answer: "answer from choices"
+    }
+  ];
